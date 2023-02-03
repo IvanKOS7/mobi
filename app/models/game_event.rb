c4306c996bea9604631d8c78e661e3a5c5605f41
IvanKOS7 <ivankos429@gmail.com>
2023-02-03 12:52:24 +0700

    fix

--- app/models/game_event.rb
+++ app/models/game_event.rb
@@ -2,11 +2,9 @@ class GameEvent < ApplicationRecord
   belongs_to :player
   belongs_to :game
 
+  # uniq combi games attr ids
+  
   validates :player_id, presence: true, uniqueness: { scope: :game_id }
-
-  def self.top_five_world_players(param)
-    Player.joins(:player_statistic).merge(PlayerStatistic.order(param: :desc).first(5))
-  end
 
   def compare_with_achieve
     achieve = Achievement.where(event: event)
--- app/models/player.rb
+++ app/models/player.rb
@@ -8,16 +8,16 @@ class Player < ApplicationRecord
   has_and_belongs_to_many :achievements
 
   # проверить выполнил ли игрок конкретный показатель хотя бы 1 раз за предыдущие 5 матчей команды
-
+  
   def complete_achieve_for_five_games?(achieve)
     game_events.first(5).any? { |event| achieve.matches?(event) }
   end
 
   def self.top_five_players(achieve, team_id)
-    Team.find(team_id).players.joins(:game_events).merge(GameEvent.where(event: achieve.event).order(count: :desc))
+    Team.find(team_id).players.joins(:game_events).merge(GameEvent.where(event: achieve.event).order(count: :desc).first(5))
   end
 
   def self.top_world_five_players(achieve)
-    Player.all.joins(:game_events).merge(GameEvent.where(event: achieve.event).order(count: :desc))
+    Player.all.joins(:game_events).merge(GameEvent.where(event: achieve.event).order(count: :desc).first(5))
   end
 end
